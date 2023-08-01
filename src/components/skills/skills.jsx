9f2c0beeb2d813da8d5e419f1e69761bcac3c7cc
EricIllanes <eric.illanes@organizame.cl>
2023-08-01 14:20:12 -0400

    feat: refactor skills component

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,15 +1,38 @@
-import css3 from "../../icons/css3.png";
-import express from "../../icons/express.png";
-import github from "../../icons/github.png";
-import html from "../../icons/html.png";
-import javascript from "../../icons/javascript.png";
-import nodejs from "../../icons/nodejs.png";
-import react from "../../icons/react.png";
-import redux from "../../icons/redux.png";
-import postgresql from "../../icons/postgresql.png";
-import sequelize from "../../icons/sequelize.png";
-import mongodb from "../../icons/mongodb.png";
-import "./skills.css";
+import React from 'react';
+import './skills.css';
+
+import css3 from '../../icons/css3.png';
+import express from '../../icons/express.png';
+import github from '../../icons/github.png';
+import html from '../../icons/html.png';
+import javascript from '../../icons/javascript.png';
+import nodejs from '../../icons/nodejs.png';
+import react from '../../icons/react.png';
+import redux from '../../icons/redux.png';
+import postgresql from '../../icons/postgresql.png';
+import sequelize from '../../icons/sequelize.png';
+import mongodb from '../../icons/mongodb.png';
+
+const skillsData = [
+  { image: html, name: 'HTML' },
+  { image: css3, name: 'CSS' },
+  { image: react, name: 'React' },
+  { image: redux, name: 'Redux' },
+  { image: javascript, name: 'JavaScript' },
+  { image: nodejs, name: 'Node.js' },
+  { image: postgresql, name: 'PostgreSQL' },
+  { image: mongodb, name: 'MongoDB' },
+  { image: sequelize, name: 'Sequelize' },
+  { image: express, name: 'Express' },
+  { image: github, name: 'GitHub' },
+];
+
+const SkillItem = ({ image, name }) => (
+  <div className="image-text">
+    <img className="image-skill" src={image} alt={name} />
+    <span className="text-skill">{name}</span>
+  </div>
+);
 
 const Skills = () => {
   return (
@@ -18,60 +41,9 @@ const Skills = () => {
         <span className="skills-title">Skills</span>
       </div>
       <div className="skills-container">
-        <div className="image-text">
-          <img className="image-skill" src={html} alt="css3" />
-          <span className="text-skill">HTML</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={css3} alt="css3" />
-          <span className="text-skill">CSS</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={react} alt="css3" />
-          <span className="text-skill">React</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={redux} alt="css3" />
-          <span className="text-skill">Redux</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={javascript} alt="css3" />
-          <span className="text-skill">JavaScript</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={nodejs} alt="css3" />
-          <span className="text-skill">Node.js</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={postgresql} alt="css3" />
-          <span className="text-skill">PostgreSQL</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={mongodb} alt="css3" />
-          <span className="text-skill">MongoDB</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={sequelize} alt="css3" />
-          <span className="text-skill">Sequelize</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={express} alt="css3" />
-          <span className="text-skill">Express</span>
-        </div>
-
-        <div className="image-text">
-          <img className="image-skill" src={github} alt="css3" />
-          <span className="text-skill">GitHub</span>
-        </div>
+        {skillsData.map((skill, index) => (
+          <SkillItem key={index} image={skill.image} name={skill.name} />
+        ))}
       </div>
     </div>
   );
